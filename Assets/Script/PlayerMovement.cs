cd994cb0b69a7fdb4ad5a803bf36f6f89ca1c1b8
jeroen <jeroen_kruyt@hotmail.com>
2021-09-20 21:36:54 +0200

    bug fixes
    fixed jumping 
    doesnt stop gravity anymore and lets you normaly jump

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
     
     public float speed = 12f;
     public float gravity = -9.81f;
-    public float jumpHeight = -2f;
+    public float jumpHeight = 3f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -21,18 +21,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        Movement();
-        
-    }
-
-    void Movement()
-    {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f;
+            velocity.y = 0f;
         }
         
         float x = Input.GetAxis("Horizontal");
@@ -50,7 +43,9 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+        
     }
+
 }
 
 
