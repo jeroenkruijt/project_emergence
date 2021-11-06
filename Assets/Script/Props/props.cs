8d9003145d064488ff065965bb5d39f90556e29f
ahmadann <83085184+ahmadann@users.noreply.github.com>
2021-11-06 14:12:00 +0100

    Props can be destroyed nowwww

---
+++ Assets/Script/Props/props.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class props : MonoBehaviour
+{
+    [SerializeField]
+    private int propsDamage = 100;
+    private int currentDamage;
+  
+    void Start()
+    {
+        currentDamage = propsDamage;
+    }
+
+    public void BreakProp(int damageAmount)
+    {
+        currentDamage -= damageAmount;
+        if (currentDamage <= 0)
+        {
+            Debug.Log("destroyed");
+            Destroy(gameObject);
+        }
+    }
+}
